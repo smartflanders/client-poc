448be14c452980afff79c4579c6b0595a940bae0
Arne Gevaert <arne.gevaert@ugent.be>
2017-04-25 16:24:43 +0200

    classes based on trends

--- src/App.js
+++ src/App.js
@@ -18,12 +18,15 @@ class App extends Component {
     };
 
     this.state = {
-      "https://stad.gent/id/parking/P7": {recordings: {}},
-      "https://stad.gent/id/parking/P1": {recordings: {}},
-      "https://stad.gent/id/parking/P10": {recordings: {}},
-      "https://stad.gent/id/parking/P4": {recordings: {}},
-      "https://stad.gent/id/parking/P8": {recordings: {}},
-      "https://stad.gent/id/parking/P2": {recordings: {}}
+      parkings: {
+        "https://stad.gent/id/parking/P7": {recordings: {}},
+        "https://stad.gent/id/parking/P1": {recordings: {}},
+        "https://stad.gent/id/parking/P10": {recordings: {}},
+        "https://stad.gent/id/parking/P4": {recordings: {}},
+        "https://stad.gent/id/parking/P8": {recordings: {}},
+        "https://stad.gent/id/parking/P2": {recordings: {}}
+      },
+      intervalBorders: []
     };
 
     let consumer = new Consumer({parallel: false});
@@ -33,20 +36,19 @@ class App extends Component {
     });
     let from = moment().subtract(15, 'minutes').format('YYYY-MM-DDTHH:mm:ss');
     consumer.getInterval(from);
-    console.log(from);
   }
 
   processTriple(triple) {
-    if (this.state[triple.subject] !== undefined) {
-      let parking = this.state[triple.subject];
+    if (this.state.parkings[triple.subject] !== undefined) {
+      let parking = this.state.parkings[triple.subject];
       if (triple.predicate === this.buildingBlocks.predicates.description) {
         parking.description = this.extractLiteral(triple.object);
-        this.setState({[triple.subject]: parking});
+        this.setState({parkings: Object.assign(this.state.parkings, {[triple.subject]: parking})});
       }
       if (triple.predicate === this.buildingBlocks.predicates.vacantSpaces) {
         parking.recordings[triple.graph.substr(triple.graph.length - this.momentStringLength)] = triple.object;
         parking.delta = this.calculateSpaceDelta(parking);
-        this.setState({[triple.subject]: parking});
+        this.setState({parkings: Object.assign(this.state.parkings, {[triple.subject]: parking}), intervalBorders: this.calculateClassIntervalBorders()});
       }
     }
   }
@@ -66,17 +68,50 @@ class App extends Component {
     }
   }
 
+  calculateClassIntervalBorders() {
+    let deltas = [];
+    deltas.sort();
+    Object.keys(this.state.parkings).forEach((key) => {
+      deltas.push(this.state.parkings[key].delta);
+    });
+    let diff = deltas[deltas.length-1] - deltas[0];
+    let intervalLength = Math.round(diff/3);
+    return [deltas[0] + intervalLength, deltas[0] + 2*intervalLength]
+  }
+
+  getClassForDelta(delta) {
+    let borders = this.state.intervalBorders;
+    if (borders) {
+      if (delta < borders[0]) {
+        return "panel-danger";
+      } else if (delta < borders[1]) {
+        return "panel-info";
+      } else {
+        return "panel-success";
+      }
+    }
+    return "panel-info";
+  }
+
   render() {
     return (
       <div className="App">
         <nav className="navbar navbar-default">
           <a className="navbar-brand" href="#">SmartFlanders: Proof of concept</a>
         </nav>
-        {Object.keys(this.state).map((el, i) => {
+        {Object.keys(this.state.parkings).map((el, i) => {
           return <ParkingPanel
-            description={this.state[el].description}
-            delta={this.state[el].delta}/>
+            description={this.state.parkings[el].description}
+            delta={this.state.parkings[el].delta}
+            class={this.getClassForDelta(this.state.parkings[el].delta)}
+            key={i}/>
         })}
+        <div>
+          {this.state.intervalBorders[0]}
+        </div>
+        <div>
+          {this.state.intervalBorders[1]}
+        </div>
       </div>
 
 
--- src/ParkingPanel.js
+++ src/ParkingPanel.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 
 class ParkingPanel extends Component {
-  constructor(props) {
-    super(props);
-    this.panelClass = "panel-primary";
-  }
-
   render() {
     return (
       <div className="col-sm-4">
-        <div className={"panel panel-default " + this.panelClass}>
+        <div className={"panel panel-default " + this.props.class}>
           <div className="panel-heading">
             <h3 className="panel-title">{this.props.description}</h3>
           </div>
